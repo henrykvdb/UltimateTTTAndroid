cb869a4d1e39d858b6bf7070b318ef58ab0407aa
Henrykvdb <henrykvdb@hotmail.com>
2018-02-20 19:14:02 +0100

    Fixed sending empty boards over bt and bumped version to 2.1.0

--- app/src/main/java/com/flaghacker/sttt/common/JSONBoard.kt
+++ app/src/main/java/com/flaghacker/sttt/common/JSONBoard.kt
@@ -4,27 +4,27 @@ import org.json.JSONArray
 import org.json.JSONObject
 
 fun Board.toJSON(): JSONObject {
-	val json = JSONObject()
-	val jsonBoard = JSONArray()
-	for (i in 0 until 81) jsonBoard.put(tile(i.toByte()).toNiceString())
+    val json = JSONObject()
+    val jsonBoard = JSONArray()
+    for (i in 0 until 81) jsonBoard.put(tile(i.toByte()).toNiceString())
 
-	json.put("board", jsonBoard)
-	json.put("macroMask", macroMask())
-	json.put("lastMove",lastMove()?.toInt())
+    json.put("board", jsonBoard)
+    json.put("macroMask", macroMask())
+    json.put("lastMove", lastMove()?.toInt())
 
-	return json
+    return json
 }
 
 class JSONBoard {
-	companion object {
-		fun fromJSON(json: JSONObject): com.flaghacker.sttt.common.Board {
-			val board = Array(9, { Array(9, { Player.NEUTRAL }) })
-			for (i in 0 until 81)
-				board[i.toPair().first][i.toPair().second] = fromNiceString(json.getJSONArray("board").getString(i))
-			val macroMask = json.getInt("macroMask")
-			val lastMove = json.getInt("lastMove")
+    companion object {
+        fun fromJSON(json: JSONObject): com.flaghacker.sttt.common.Board {
+            val board = Array(9, { Array(9, { Player.NEUTRAL }) })
+            for (i in 0 until 81)
+                board[i.toPair().first][i.toPair().second] = fromNiceString(json.getJSONArray("board").getString(i))
+            val macroMask = json.getInt("macroMask")
+            val lastMove = if (json.length() == 3) json.getInt("lastMove") else null
 
-			return Board(board, macroMask, lastMove.toByte())
-		}
-	}
+            return Board(board, macroMask, lastMove?.toByte())
+        }
+    }
 }
